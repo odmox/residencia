94ddae247c471c29e1877864f2cde6b41acdf57f
Moreno Ortega Oscar David <42548416+odmox@users.noreply.github.com>
2023-04-20 17:13:41 +0000

    Oauth Implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <Auth0Provider  
+      domain={domain} 
+      clientId={clientId}
+      redirectUri={ "/" }
+    >    
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
 }
